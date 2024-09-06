d8a5e342ed328872c700a58cb72aa205c4f89ca1
Vinitkumar Gaikwad <vinit.kumar@currencysolutions.com>
2024-09-06 07:56:57 +0100

    Static files served

--- main.go
+++ main.go
@@ -2,16 +2,54 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
+func getFileList(dir string) ([]string, error) {
+	dirFs, err := os.Open(dir)
+	if err != nil {
+		return nil, err
+	}
+	files, err := dirFs.ReadDir(0)
+	if err != nil {
+		return nil, err
+	}
+
+	outArr := []string{}
+	for _, file := range files {
+		outArr = append(outArr, strings.Split(file.Name(), ".")[0])
+	}
+	return outArr, nil
+}
+
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-	log.Println("Got request on ", r.URL.Path)
-	fmt.Fprintf(w, "<h1>Welcome to E-Manuals Home Page</h1>")
+	tmp, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Required resource not found: %s", err.Error())
+		return
+	}
+	files, err := getFileList("static/manuals")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "No data to load: %s", err.Error())
+		return
+	}
+
+	tmp.Execute(w, files)
 }
 
 func main() {
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./static/css"))))
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./static/images"))))
+	http.Handle("/manuals/", http.StripPrefix("/manuals/", http.FileServer(http.Dir("./static/manuals"))))
+
 	http.HandleFunc("/", handleRoot)
+
+	log.Println("listening on port 3000...")
 	http.ListenAndServe(":3000", nil)
 }
